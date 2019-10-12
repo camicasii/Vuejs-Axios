58dd2159120d8957e57219ccb819c7d5003602f9
camicasii <camicasii@hotmail.com>
2019-10-12 13:30:46 -0400

    probando props de funciones

--- src/App.vue
+++ src/App.vue
@@ -3,55 +3,17 @@
   <v-app>
     <v-content>
       <v-container>
-      {{picker}} and {{picker2}} and
-      {{max}}
-      <v-row   class="m-5">
-      <v-card class="mr-5">      
-      <v-date-picker v-model="picker2" color="green lighten-1 " header-color="primary"
-      locale="es"
-      :min="min"      
-      ></v-date-picker>
-      </v-card>    
-      <v-card>
-      <v-date-picker v-model="picker" color="green lighten-1 "></v-date-picker>
       
-      </v-card>    
-      <v-card
-    class="mx-auto"
-    max-width="400"   
-  >
-    <v-img
-      class="white--text align-end"
-      height="200px"
-      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-    >
-      <v-card-title>Top 10 Australian beaches</v-card-title>
-    </v-img>
-
-
-
-    <v-card-text class="text--primary">
-      <div>Whitehaven Beach</div>
-
-      <div>Whitsunday Island, Whitsunday Islands</div>
+      <v-row   class="m-5">
+  <CardDolar :dataProps="picker2" :callback="changeData"/>
+  <v-card class="ml-5">
+    <v-card-text>
+    <h2>{{picker2}}</h2>
     </v-card-text>
-
-    <v-card-actions>
-      <v-btn
-        color="orange"
-        text
-      >
-        Share
-      </v-btn>
-
-      <v-btn
-        color="orange"
-        text
-      >
-        Explore
-      </v-btn>
-    </v-card-actions>
   </v-card>
+
+          
+   
       
       
       </v-row>
@@ -67,30 +29,22 @@
 <script>
 //todo lo que se trabaje con Vuetify debe estar contenido en v-app
 import axios  from "axios";
+import CardDolar from "./components/CardDolar.vue";
 
 export default {
+  components:{
+  CardDolar
+  },
   name: 'App',
-  data: () => ({
-    picker: new Date().toISOString().substr(0, 10),
-    picker2: new Date().toISOString().substr(0, 10),
-    min:"1984-12-15",
-    max:"2019-12-15",
-    date: '2018-03-02'
-  }), methods: {
-      allowedDates: val => parseInt(val.split('-')[2], 10) % 2 === 0,
-      async getApi(){
-        const data = await axios("https://mindicador.cl/api/dolar")
-                            .then(res=>res.data)
-        console.log(data);
-          
+  data: () => ({    
+    picker2: new Date().toISOString().substr(0, 10)
+  }),
+    methods: {
+      changeData(data){
+        //this.picker2 = data
+        console.log("my metodo",data);
         
       }
     },
-    created() {
-      this.getApi()
-    },
-  
-    
-  
 };
 </script>
---
+++ src/components/CardDolar.vue
@@ -0,0 +1,67 @@
+<template >
+    <div>
+    <v-card class="mr-5" color="pink">      
+      <v-date-picker v-model="picker2" color="green lighten-1 " header-color="primary"
+      locale="es"
+      :min="min" 
+      :max="max"
+      :change="getApiDay(picker2)"     
+      full-width
+      />
+      <v-card-text  class="text-center">
+      <h2> Valor del Dolar: <span
+      v-if="bool">{{valor}}$</span>
+      <span
+      v-else="bool">{{sms}}</span>
+      </h2>
+      
+      </v-card-text>
+      </v-card>    
+        
+    </div>
+</template>
+
+<script>
+//todo lo que se trabaje con Vuetify debe estar contenido en v-app
+import axios  from "axios";
+
+export default {
+  name: 'CardDolar',
+  data: () => ({    
+    picker2:"2011-05-12",
+    min:"1984-12-15",
+    max:new Date().toISOString().substr(0, 10),    
+    valor:"",
+    bool:false,
+    sms:"No hay valores disponibles para esta fecha"
+
+
+  }),  props: {
+      dataProps:String
+  , 
+  callback:Function
+  },
+  methods: {  
+      async getApiDay(date){
+          console.log(this.dataProps);
+          this.metodo("paso")
+          
+        const endPoint=date.split("-").reverse().join("-")
+        const data = await axios("https://mindicador.cl/api/dolar/"+endPoint)
+                            .then(res=>res.data.serie[0])
+                            if(data !=undefined){
+                            this.valor= data.valor
+                            this.bool=true;
+
+                            }
+                            else(
+                              //this.valor= "Precio no disponible"
+                              this.bool=false
+                            )
+                            
+      }
+    
+    }
+    
+};
+</script>
